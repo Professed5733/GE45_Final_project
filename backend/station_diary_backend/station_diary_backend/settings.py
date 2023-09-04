6a419918a70d3374a6506518451fabd43b27aafb
oyster2387 <mohamedf2_net@hotmail.com>
2023-09-04 15:52:03 +0800

    Updated gitignore

--- backend/station_diary_backend/station_diary_backend/settings.py
+++ backend/station_diary_backend/station_diary_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,7 +77,11 @@ WSGI_APPLICATION = 'station_diary_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'stationdb',
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
