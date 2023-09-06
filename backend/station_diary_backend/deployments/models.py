981a6ea8800102da39319bb854b9a5ce27c1a30f
oyster2387 <mohamedf2_net@hotmail.com>
2023-09-06 14:30:55 +0800

    Created Deployment app and deployment models

---
+++ backend/station_diary_backend/deployments/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from station_diary_backend.users.models import Account, PoliceStation
+import uuid
+
+
+# Create your models here.
+
+class Shift(models.Model):
+    shift = models.CharField(max_length=255, primary_key=True)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return self.shift
+
+
+class Sector(models.Model):
+    sector = models.CharField(max_length=255, primary_key=True)
+    station = models.ForeignKey(PoliceStation, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.sector
+
+class Deployment(models.Model):
+    deployment_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    shift = models.ForeignKey(Shift, on_delete=models.CASCADE)
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+    users = models.ManyToManyField(Account)
+    is_active = models.BooleanField(default=True)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.deployment_id
