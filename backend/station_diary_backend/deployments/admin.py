f3708db4b62a0ebdfa6e37a915a9e26df3403c1a
oyster2387 <mohamedf2_net@hotmail.com>
2023-09-07 15:28:44 +0800

    Basic admin panel completed

--- backend/station_diary_backend/deployments/admin.py
+++ backend/station_diary_backend/deployments/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Shift, Sector
 
-# Register your models here.
+class ShiftAdmin(admin.ModelAdmin):
+    list_display = ('shift', 'start_time', 'end_time')
+    search_fields = ('shift',)
+    list_filter = ('start_time', 'end_time')
+    ordering = ('shift',)
+    fieldsets = (
+        (None, {'fields': ('shift', 'start_time', 'end_time')}),
+    )
+
+class SectorAdmin(admin.ModelAdmin):
+    list_display = ('sector', 'station', 'description')
+    search_fields = ('sector', 'station__station', 'description')
+    list_filter = ('station',)
+    ordering = ('sector',)
+    fieldsets = (
+        (None, {'fields': ('sector', 'station', 'description')}),
+    )
+
+admin.site.register(Shift, ShiftAdmin)
+admin.site.register(Sector, SectorAdmin)
--- backend/station_diary_backend/loggings/admin.py
+++ backend/station_diary_backend/loggings/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Tencode, PINType, ContactType, SubjectStatus
 
-# Register your models here.
+class TencodeAdmin(admin.ModelAdmin):
+    list_display = ('tencode', 'description')
+    search_fields = ('tencode', 'description')
+    ordering = ('tencode',)
+
+class PINTypeAdmin(admin.ModelAdmin):
+    list_display = ('PIN_Type',)
+    search_fields = ('PIN_Type',)
+    ordering = ('PIN_Type',)
+
+class ContactTypeAdmin(admin.ModelAdmin):
+    list_display = ('contact_type',)
+    search_fields = ('contact_type',)
+    ordering = ('contact_type',)
+
+class SubjectStatusAdmin(admin.ModelAdmin):
+    list_display = ('subject_status',)
+    search_fields = ('subject_status',)
+    ordering = ('subject_status',)
+
+admin.site.register(Tencode, TencodeAdmin)
+admin.site.register(PINType, PINTypeAdmin)
+admin.site.register(ContactType, ContactTypeAdmin)
+admin.site.register(SubjectStatus, SubjectStatusAdmin)
