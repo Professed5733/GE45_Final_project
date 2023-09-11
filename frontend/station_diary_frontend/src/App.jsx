45ffe3bbffd2c94b2e6d2c33fd5aaa05145d7491
oyster2387 <mohamedf2_net@hotmail.com>
2023-09-11 10:25:15 +0800

    Installed Material UI and Navbar

--- frontend/station_diary_frontend/src/App.jsx
+++ frontend/station_diary_frontend/src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Display from "./components/Display";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Display />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/station_diary_frontend/src/components/Display.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Display = () => {
+  return <>Display Component</>;
+};
+
+export default Display;
